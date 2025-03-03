66fe2524ff5328ce28a3c01121ea00474f9e3c1a
PolinaPolupan <polupanpolina9@gmail.com>
2025-03-03 19:54:35 +0300

    feat: Init spring boot project

---
+++ src/main/java/com/example/MyPixelApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPixelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPixelApplication.class, args);
+    }
+
+}
