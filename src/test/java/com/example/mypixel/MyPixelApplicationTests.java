10f204b3382110ae0e10e1cec93dd73a9fb22573
PolinaPolupan <polupanpolina9@gmail.com>
2025-03-04 11:30:42 +0300

    ci: Setup gradle ci

---
+++ src/test/java/com/example/mypixel/MyPixelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mypixel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPixelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
