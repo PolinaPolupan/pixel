fedde91075a91a9a2b915aac01013d2148ddff8d
PolinaPolupan <polupanpolina9@gmail.com>
2025-04-03 19:43:39 +0300

    Reorganize project structure

---
+++ native/cmake-build-debug-wsl/CMakeFiles/native-processor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/native-processor.dir/link.d"
+  "CMakeFiles/native-processor.dir/src/filtering.cpp.o"
+  "CMakeFiles/native-processor.dir/src/filtering.cpp.o.d"
+  "libnative-processor.pdb"
+  "libnative-processor.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/native-processor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
