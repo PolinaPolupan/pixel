01310584b3be5cae9648a17cb34c9b53139b938f
PolinaPolupan <polupanpolina9@gmail.com>
2025-06-23 14:02:55 +0300

    refactor: Remove folders

--- native/cmake-build-debug-wsl/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/polina/dev/java/mypixel/native
-# Build directory: /home/polina/dev/java/mypixel/native/cmake-build-debug-wsl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
