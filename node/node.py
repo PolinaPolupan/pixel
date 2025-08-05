f9652da9c6479ed9c986c536fdf30139b4dbc46c
PolinaPolupan <polupanpolina9@gmail.com>
2025-08-05 18:23:40 +0300

    refactor: merge exec and validate params methods

--- node/node.py
+++ node/node.py
@@ -1,10 +1,39 @@
 import inspect
 from abc import ABC, abstractmethod
+from inspect import Signature
 from typing import Any, Dict, Optional, Type
 
 from metadata import Metadata
 
 NODE_REGISTRY = {}
+
+
+def map_params(inputs: Dict[str, Any], sig: Signature) -> Dict[str, Any]:
+    input_data = inputs.get("inputs", inputs)
+    meta_data = inputs.get("meta", {})
+
+    meta = Metadata(
+        id=meta_data.get("node_id"),
+        scene_id=meta_data.get("scene_id"),
+        task_id=meta_data.get("task_id")
+    )
+
+    params = {}
+
+    for param_name, param in sig.parameters.items():
+        if param_name == 'self':
+            continue
+        elif param_name == 'meta':
+            params[param_name] = meta
+        elif param_name in input_data:
+            params[param_name] = input_data[param_name]
+        elif param.default is not inspect.Parameter.empty:
+            continue
+        else:
+            params[param_name] = None
+
+    return params
+
 
 class Node(ABC):
     node_type = None
@@ -26,62 +55,16 @@ class Node(ABC):
         pass
 
     def exec_params(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
-        input_data = inputs.get("inputs", inputs)
-        meta_data = inputs.get("meta", {})
-
-        meta = Metadata(
-            id=meta_data.get("id"),
-            scene_id=meta_data.get("sceneId"),
-            task_id=meta_data.get("taskId")
-        )
-
         sig = inspect.signature(self.exec)
-        params = {}
-
-        for param_name, param in sig.parameters.items():
-            if param_name == 'self':
-                continue
-            elif param_name == 'meta':
-                params[param_name] = meta
-            elif param_name in input_data:
-                params[param_name] = input_data[param_name]
-            elif param.default is not inspect.Parameter.empty:
-                continue
-            else:
-                params[param_name] = None
-
-        return self.exec(**params)
+        return self.exec(**map_params(inputs, sig))
 
     @abstractmethod
     def exec(self, **kwargs) -> Dict[str, Any]:
         pass
 
     def validate_params(self, inputs: Dict[str, Any]) -> None:
-        input_data = inputs.get("inputs", inputs)
-        meta_data = inputs.get("meta", {})
-
-        meta = Metadata(
-            id=meta_data.get("id"),
-            scene_id=meta_data.get("sceneId"),
-            task_id=meta_data.get("taskId")
-        )
-
         sig = inspect.signature(self.validate)
-        params = {}
-
-        for param_name, param in sig.parameters.items():
-            if param_name == 'self':
-                continue
-            elif param_name == 'meta':
-                params[param_name] = meta
-            elif param_name in input_data:
-                params[param_name] = input_data[param_name]
-            elif param.default is not inspect.Parameter.empty:
-                continue
-            else:
-                params[param_name] = None
-
-        return self.validate(**params)
+        return self.validate(**map_params(inputs, sig))
 
     @abstractmethod
     def validate(self, **kwargs) -> None:
