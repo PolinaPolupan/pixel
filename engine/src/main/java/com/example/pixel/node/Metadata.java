d9b4d54d34f316ea5c3bf678e8d5b2ed9157449f
PolinaPolupan <polupanpolina9@gmail.com>
2025-08-19 17:34:56 +0300

    refactor: add getters for serialization

--- engine/src/main/java/com/example/pixel/node/Metadata.java
+++ engine/src/main/java/com/example/pixel/node/Metadata.java
@@ -3,7 +3,9 @@ package com.example.pixel.node;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Metadata {
--- engine/src/main/java/com/example/pixel/node/NodeData.java
+++ engine/src/main/java/com/example/pixel/node/NodeData.java
@@ -1,11 +1,13 @@
 package com.example.pixel.node;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.Map;
 
+@Getter
 @AllArgsConstructor
 public class NodeData {
     private Metadata meta;
-    private Map<String, Object> data;
+    private Map<String, Object> inputs;
 }
