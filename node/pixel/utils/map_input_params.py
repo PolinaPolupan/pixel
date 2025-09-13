78408a4427e1f54234379713c19f9a89378e7e9f
PolinaPolupan <polupanpolina9@gmail.com>
2025-09-13 18:28:52 +0300

    refactor: fix packages

--- node/pixel/utils/map_input_params.py
+++ node/pixel/utils/map_input_params.py
@@ -2,7 +2,7 @@ import inspect
 from inspect import Signature
 from typing import Dict, Any
 
-from pixel.models import Metadata
+from pixel.models.metadata import Metadata
 
 
 def map_input_params(inputs: Dict[str, Any], sig: Signature) -> Dict[str, Any]:
